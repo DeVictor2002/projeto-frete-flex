e6e5caecfb4924eadd7e00af2c74535e229eaeb3
Victor <victorhenriquebl11@gmail.com>
2025-02-27 20:11:43 -0300

    feat(calculate): Implementing calculate method in the Service

--- src/main/java/tech/devictor/freteflex/service/ShippingService.java
+++ src/main/java/tech/devictor/freteflex/service/ShippingService.java
@@ -1,13 +1,29 @@
 package tech.devictor.freteflex.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import tech.devictor.freteflex.domain.ShippingCalculator;
 
 @Service
 public class ShippingService {
 
+    private final ShippingCalculator standardShippingCalculator;
+    private final ShippingCalculator expressShippingCalculator;
+
+    public ShippingService(@Qualifier("standardShippingCalculator") ShippingCalculator standardShippingCalculator,
+                           @Qualifier("expressShippingCalculator") ShippingCalculator expressShippingCalculator) {
+        this.standardShippingCalculator = standardShippingCalculator;
+        this.expressShippingCalculator = expressShippingCalculator;
+    }
+
     public Double calculate(String shippingType,
                             Double distance,
                             Double weight) {
-        return 0.0;
+        if (shippingType.equalsIgnoreCase("standatd")) {
+            return standardShippingCalculator.calculate(distance, weight);
+        } else if (shippingType.equalsIgnoreCase("express")) {
+            return expressShippingCalculator.calculate(distance, weight);
+        }
+        return null;
     }
 }
