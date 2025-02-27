e4655e9e823313f2330e326e6a3e67778bd9277c
Victor <victorhenriquebl11@gmail.com>
2025-02-27 19:51:46 -0300

    feat(calculate): Creating ShippingService and DI constructor in the ShippingController

--- src/main/java/tech/devictor/freteflex/controller/ShippingController.java
+++ src/main/java/tech/devictor/freteflex/controller/ShippingController.java
@@ -5,10 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.devictor.freteflex.controller.dto.ShippingResponse;
+import tech.devictor.freteflex.service.ShippingService;
 
 @RestController
 
 public class ShippingController {
+
+    private final ShippingService shippingService;
+
+    public ShippingController(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
 
     @GetMapping(value = "/shipping/calculate")
     public ResponseEntity<ShippingResponse> calculate(@RequestParam("type") String shippingType,
