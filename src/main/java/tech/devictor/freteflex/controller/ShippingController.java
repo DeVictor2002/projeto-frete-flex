8f9740849b9bc936db006efd48140d9e9a7f06d4
Victor <victorhenriquebl11@gmail.com>
2025-02-27 19:45:32 -0300

    test(calculate): Making HTTP requests on the endpoint /shipping/calculate

--- src/main/java/tech/devictor/freteflex/controller/ShippingController.java
+++ src/main/java/tech/devictor/freteflex/controller/ShippingController.java
@@ -4,15 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tech.devictor.freteflex.controller.dto.ShippingResponse;
 
 @RestController
 
 public class ShippingController {
 
     @GetMapping(value = "/shipping/calculate")
-    public ResponseEntity<?> calculate(@RequestParam("type") String shippingType,
-                                       @RequestParam("distance") Double distance,
-                                       @RequestParam("weight") Double weight) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ShippingResponse> calculate(@RequestParam("type") String shippingType,
+                                                      @RequestParam("distance") Double distance,
+                                                      @RequestParam("weight") Double weight) {
+        return ResponseEntity.ok(new ShippingResponse(0.0));
     }
 }
